be4219edf2ae0a9bded5013f344879eda3fde8fe
sha-abhishek <asharm5@clemson.edu>
2022-11-28 19:10:50 -0500

    Training Comparison PPO vs DQN

--- scripts/sb3_highway_ppo.py
+++ scripts/sb3_highway_ppo.py
@@ -37,12 +37,14 @@ if __name__ == "__main__":
         # Save the agent
         model.save("highway_ppo/model")
 
-    model = PPO.load("highway_ppo/model")
-    env = gym.make("highway-fast-v0")
-    for _ in range(5):
-        obs, info = env.reset()
-        done = truncated = False
-        while not (done or truncated):
-            action, _ = model.predict(obs)
-            obs, reward, done, truncated, info = env.step(action)
-            env.render()
+
+model = PPO.load("highway_ppo/model")
+env = gym.make("highway-fast-v0")
+for _ in range(5):
+    #obs, info = env.reset()
+    obs = env.reset()
+    done = truncated = False
+    while not (done or truncated):
+        action, _ = model.predict(obs)
+        obs, reward, done, info = env.step(int(action))
+        env.render()
